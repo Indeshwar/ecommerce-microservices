9ad65574e196977129644c352f946217998f362a
Indeshwar <indeshc@gmail.com>
2025-01-23 11:53:22 -0600

    ecommerce microservices project created

---
+++ order-service/src/main/java/com/demo/controller/OrderController.java
@@ -0,0 +1,14 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class OrderController {
+    @GetMapping("/message")
+    public String getMessage(){
+        return "This is Order Service";
+    }
+}
