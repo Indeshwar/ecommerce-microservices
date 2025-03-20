ba119d1a759836ca7b5df86de800924694643b4d
Indeshwar <indeshc@gmail.com>
2025-03-20 15:06:33 -0500

    Done Integration Test

--- inventry-service/src/test/java/com/demo/controller/TestInventoryController.java
+++ inventry-service/src/test/java/com/demo/controller/TestInventoryController.java
@@ -12,10 +12,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestTemplate;
 
 
-import java.lang.reflect.Type;
+
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -45,11 +46,17 @@ public class TestInventoryController {
     @Test
     void testSaveInventory(){
 
-        InventoryRequest request = new InventoryRequest();
-        request.setInventoryName("Iphone");
-        request.setCategory("Phone");
-        request.setQuantity(4L);
-        request.setPrice(1000.0);
+        InventoryRequest request1 = new InventoryRequest();
+        request1.setInventoryName("Iphone 15");
+        request1.setCategory("IPhone");
+        request1.setQuantity(4L);
+        request1.setPrice(1000.0);
+
+        InventoryRequest request2 = new InventoryRequest();
+        request2.setInventoryName("Samsung Galaxy");
+        request2.setCategory("Samsung");
+        request2.setQuantity(4L);
+        request2.setPrice(1000.0);
 
         //Create an Object of HttpHeaders
         HttpHeaders headers = new HttpHeaders();
@@ -58,15 +65,38 @@ public class TestInventoryController {
         headers.set("authority", "USER");
 
         //Create a HttpEntity Object
-        HttpEntity<InventoryRequest> httpEntity = new HttpEntity<>(request, headers);
-        ResponseEntity<String> response = restTemplate.exchange(
-                baseUrl + "/save", HttpMethod.POST, httpEntity, String.class);
+        HttpEntity<InventoryRequest> httpEntity_1 = new HttpEntity<>(request1, headers);
+        HttpEntity<InventoryRequest> httpEntity_2 = new HttpEntity<>(request2, headers);
+        ResponseEntity<String> response = null;
+        for(int i = 0; i < 4; i++){
+            if(i == 0){
+                response = restTemplate.exchange(
+                        baseUrl + "/save", HttpMethod.POST, httpEntity_1, String.class);
+            }else{
+                response = restTemplate.exchange(
+                        baseUrl + "/save", HttpMethod.POST, httpEntity_2, String.class);
+            }
+        }
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals("data successfully saved", response.getBody());
 
+    }
 
+    @Test
+    void testFindInventoryByCategory(){
+        InventoryRequest request = new InventoryRequest();
+        request.setCategory("IPhone");
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("userName", "Hari");
+        headers.set("authority", "USER");
+
+        HttpEntity httpEntity = new HttpEntity<>(request, headers);
+        ResponseEntity<List<Inventory>> response = restTemplate.exchange(baseUrl + "/category", HttpMethod.GET, httpEntity, new ParameterizedTypeReference<List<Inventory>>() {
+        });
+
+        assertEquals(1, response.getBody().size());
     }
 
     @Test
@@ -105,5 +135,19 @@ public class TestInventoryController {
 
         //Assert
         assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    void TestFindByInventoryId(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("userName", "Hari");
+        headers.set("authority", "USER");
+
+        HttpEntity httpEntity = new HttpEntity(headers);
+        ResponseEntity<Inventory> response = restTemplate.exchange(baseUrl + "/inventoryId/79b85b84-8a8e-4d07-a56d-8640e9392d9a", HttpMethod.GET, httpEntity, Inventory.class);
+        System.out.println("HELLO " + response.getBody().getInventoryName());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+
     }
 }
